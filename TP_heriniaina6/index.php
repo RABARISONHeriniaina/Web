6bdea83398cb899233278168778111d878b014a4
RABARISON Heriniaina <71127940+RABARISONHeriniaina@users.noreply.github.com>
2020-09-11 12:20:35 +0200

    Add files via upload
    Kotrama MF
    Test design in 3hour

---
+++ TP_heriniaina6/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Gestion d'emploi du temps</title>
+	<meta http-equiv="refresh" content="0;vue/index.php">
+</head>
+<body>
+</body>
+</html>
---
+++ TP_heriniaina6/vue/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Gestion D'Emploi Du Temps</title>
+	<link rel="icon" type="image/icon" href="./atout/gdt.ico">
+	<link rel="stylesheet" type="text/css" href="./style/style.css">
+</head>
+<body>
+	<div class="blog">
+		<div class="formulaireLogIn">
+			<form method="POST" action="#">
+				<h1>Indentifiez-vous</h1>
+				<div class="complete">
+					<label>
+						<span>E-mail</span>
+						<input type="text" name="" placeholder="rabarisonheriniaina@gmail.com"  required="">			
+					</label>
+					<label>
+						<span>Password</span>
+						<input type="password" name="" placeholder="******************" required="">
+					</label>
+					<input type="submit" name="" value="valider">
+				</div>
+			</form>
+			
+		</div>
+	</div>
+
+</body>
+</html>
